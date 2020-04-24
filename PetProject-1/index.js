72cae626a9f90e4921283790f59466dac6f883c5
JhonCowNut <55220275+JohnCowNut@users.noreply.github.com>
2020-04-24 15:19:45 +0700

    Done express-codersx-7-bai-1

--- PetProject-1/index.js
+++ PetProject-1/index.js
@@ -10,14 +10,16 @@ const PORT = 9000;
 const adapter = new FileSync('db.json')
 const db = low(adapter)
 app.use(bodyParser.urlencoded({ extended: false }))
-db.defaults({ books: []})
+db.defaults({books: []},{users: []})
   .write()
 
-const storeBooks = db.get("books").value()
+const storeBooks = db.get("books").value();
+const storeUsers = db.get("users").value();
 // parse application/json
 app.use(bodyParser.json())
+/*BOOK*/
 app.get("/",(req,res) => {
-	res.send("<a href='/books'>See All Books</a>")
+	res.render("overView")
 })
 app.get('/books',(req,res)=> {
 	const books = [].concat(storeBooks)
@@ -44,12 +46,12 @@ app.get('/books/:id/delete',(req,res) => {
 		} 
 	}
 	db.get("books").remove(bookDelete).write();
-	res.redirect("/")
+	res.redirect("/books")
 })
 app.get("/books/:id/edit",(req,res) => {
 	const idBook = req.params.id * 1;
 	let book  = storeBooks.filter( el => el.id === idBook)
-	res.render("editTitle",{
+	res.render("editTitleBook",{
 		book: storeBooks
 	})
 })  
@@ -67,6 +69,57 @@ app.post("/books/:id/edit",(req,res) => {
 	  .assign({ title: newTitle})
 	  .write()
 	res.redirect("/")
+})
+/*USER*/
+app.get("/users",(req,res) => {
+	let currentUsers = [].concat(storeUsers)
+	res.render("users",{
+		users : currentUsers
+	})
+})
+app.get("/users/create",(req,res) => {
+	res.render("createUser")
+}) 
+app.post("/users/create",(req,res) => {
+	let newIdUser = storeUsers.length +1;
+	let query = {...req.body}
+	const newUser = Object.assign({},{id: newIdUser},query);
+	db.get("users").push(newUser).write();
+	res.redirect("/users")
+})
+app.get("/users/:id/edit",(req,res) => {
+	const idUser = req.params.id * 1;
+	let user  = storeUsers.filter( el => el.id === idUser)
+	res.render("editNameUser",{
+		users: user
+	})
+})
+app.post("/users/:id/edit",(req,res) => {
+	let newUserName = req.body.edit;
+	let idUser = req.params.id * 1;
+	let bookUpdate = {}
+	for(let i = 0 ; i < storeUsers.length ; i ++) {
+		if(idUser === storeUsers[i].id) {
+			bookUpdate = storeUsers[i]
+		} 
+	}  
+	
+	db.get('users')
+	  .find({ name: bookUpdate.name })
+	  .assign({ name: newUserName})
+	  .write()
+	res.redirect("/users")
+})
+app.get('/users/:id/delete',(req,res) => {
+	const idUser = req.params.id * 1 ; 
+	let bookDelete= {};
+	for(let i = 0 ; i < storeUsers.length ; i ++) {
+		if(idUser === storeUsers[i].id) {
+			bookDelete = storeUsers[i]
+		} 
+	}
+	db.get("users").remove(bookDelete).write();
+	res.redirect("/users")
 })
 app.listen(PORT ,(req,res)=> {
 	console.log("Server is Runing on PORT: ",PORT)
