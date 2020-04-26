484b3875bc68869e0cac9591defba17b45883029
JhonCowNut <55220275+JohnCowNut@users.noreply.github.com>
2020-04-26 12:53:27 +0700

    Fix layout boostrap look application nicer

--- PetProject-1/index.js
+++ PetProject-1/index.js
@@ -11,7 +11,7 @@ const PORT = 9000;
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.get("/",(req,res) => {
-	res.render("overView")
+	res.render("layout")
 })
 
 
