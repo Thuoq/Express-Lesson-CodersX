5dd3952f0d877a9ddde2aa3213d41f0701408ef0
JhonCowNut <55220275+JohnCowNut@users.noreply.github.com>
2020-04-26 15:13:09 +0700

    Add cookie parser and add middlawae make count cookie

--- PetProject-1/index.js
+++ PetProject-1/index.js
@@ -2,14 +2,23 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const userRouter = require("./routers/user.router");
 const bookRouter  = require("./routers/book.router");
+const cookieParser = require('cookie-parser');
 const trancationRouter = require("./routers/transcaction.router");
 const app = express();
-app.use(express.static('public'))
+app.use(express.static('public'));
+app.use(cookieParser())
 app.set('view engine', 'pug')
 app.set('views', './views')
 const PORT = 9000;
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+app.use((req,res,next) => {
+	let count = req.cookies.cookie ? req.cookies.cookie : 0 ;
+	count ++;
+	res.cookie("cookie",count);
+	console.log("cookie",count)
+	next();
+})
 app.get("/",(req,res) => {
 	res.render("layout")
 })
